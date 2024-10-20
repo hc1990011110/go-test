d3bc2bbbd150c871a03e69ff00e9535fcf09dcc8
huangchao <332330633@qq.com>
2024-10-20 11:20:46 +0800

    init

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/hc1990011110/go-test
+
+go 1.23.1
+
+require github.com/beego/beego/v2 v2.3.1
+
+require github.com/shiena/ansicolor v0.0.0-20200904210342-c7312218db18 // indirect
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/beego/beego/v2/core/logs"
+)
+
+func main() {
+	Fuckyou()
+}
+
+func Fuckyou() {
+	fmt.Println("Hello, World!")
+	logs.Info("阿西吧")
+}
